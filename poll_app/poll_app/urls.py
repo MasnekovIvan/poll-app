c7dfbdc6e011b1a5b845614e35f75c7fb6648270
Ivan <masnekovivan@gmail.com>
2021-01-11 19:18:52 +0300

    class Poll was made

--- poll_app/poll_app/urls.py
+++ poll_app/poll_app/urls.py
@@ -20,9 +20,9 @@ from poll import views as poll_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', poll_views.home, name='home')
-    path('create/', poll_views.create, name='create')
-    path('results/<poll_id>/', poll_views.results, name='results')
-    path('vote/<poll_id>/', poll_views.vote, name='vote')
-    
+    path('', poll_views.home, name='home'),
+    path('create/', poll_views.create, name='create'),
+    path('results/<poll_id>/', poll_views.results, name='results'),
+    path('vote/<poll_id>/', poll_views.vote, name='vote'),
+
 ]
