199e0590f78907378e1352d16b824fbeee72ba41
Ivan <masnekovivan@gmail.com>
2021-01-11 20:15:05 +0300

    class Poll was made

--- poll_app/poll/views.py
+++ poll_app/poll/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 
 
 from .forms import CreatePoll
@@ -6,7 +6,8 @@ from .models import Poll
 
 
 def home(request):
-    context = {}
+    polls = Poll.objects.all()
+    context = {'polls': polls}
     return render(request, 'poll/home.html', context)
 
 
@@ -24,10 +25,27 @@ def create(request):
 
 
 def results(request, poll_id):
-    context = {}
+    poll = Poll.objects.get(pk=poll_id)
+    context = {'poll': poll}
     return render(request, 'poll/results.html', context)
 
 
 def vote(request, poll_id):
-    context = {}
+    poll = Poll.objects.get(pk=poll_id)
+    if request.method == 'POST':
+        selected_option = request.POST['poll']
+        if selected_option == 'option1':
+            poll.option_one_count += 1
+        elif selected_option == 'option2':
+            poll.option_two_count += 1
+        elif selected_option == 'option3':
+            poll.option_three_count += 1
+        else:
+            return HttpResponse(400, 'invalid form')
+
+        poll.save()
+
+        return redirect('results', poll.id)
+
+    context = {'poll': poll}
     return render(request, 'poll/vote.html', context)
